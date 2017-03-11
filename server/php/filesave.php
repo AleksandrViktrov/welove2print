4a66b9178cd75cc35e2c1c65fab1b7d9153c464d
highdream-PC\highdream <alex.viktorov86@yandex.ru>
2017-03-11 16:54:25 +0800

    ok

--- server/php/filesave.php
+++
@@ -1,9 +0,0 @@
-<?php
-	$time = date( "Y_m_d_H_i_s");
-	$filename = "../../save/svg_" . $time .".svg";
-  	echo $filename;
-	$sv_str = $_POST["svg"];
-	$file = fopen($filename, "w");
-	fwrite($file, $sv_str);
-	fclose($file);
-?>
